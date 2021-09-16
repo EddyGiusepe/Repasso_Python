059a66ea3307c4bc260f992968baf68e5d1120cf
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-09-16 02:32:33 -0300

    Exemplo de lista em Python

---
+++ eddy_listas.py
@@ -0,0 +1,9 @@
+# Data Scientist Jr. Eddy Giusepe Chirinos Isidro
+# Link: https://www.youtube.com/watch?v=N1hTsbW50eM&t=2s
+print("##################################################################")
+print("## A seguir mostramos alguns exemplo do uso de Listas em PYthon ##")
+print("##################################################################")
+
+
+lista1 = [2, 5, 9, 1]
+print(lista1)
