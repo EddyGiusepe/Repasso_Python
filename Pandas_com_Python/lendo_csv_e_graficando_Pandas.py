e9b34c9543f6fc096e3df0c8ec9bf7fb5ae721b7
Karina <eddychirinos.unac@gmail.com>
2022-12-31 16:42:41 -0300

    lendo csv e graficando com o pandas

---
+++ Pandas_com_Python/lendo_csv_e_graficando_Pandas.py
@@ -0,0 +1,20 @@
+'''
+Data Scientist.: Dr.Eddy Giusepe Chirinos Isidro
+'''
+import pandas as pd
+import matplotlib.pyplot as plt
+
+"""
+Lendo um arquivo .csv com pandas e criando um pandas data frame
+"""
+dataframe = pd.read_csv('./annual-real-gnp-us-1909-to-1970.csv')
+
+plt.figure(figsize=(8, 6))
+plt.plot(dataframe['Year'], dataframe['GNP'])
+plt.xlabel('Year', size=22, c='r')
+plt.ylabel('GNP', size=22, c='r')
+plt.title('Annual Real USA GNP 1909 to 1970', size=25, c='b')
+plt.grid(True)
+#plt.savefig('annual-real-gnp-us-1909-to-1970.png')
+plt.show()
+plt.close()
