814239525a042d09c825991ffdb86a192263ebac
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-02-04 02:59:26 -0300

    Mais um exemplo de classe Pessoa

--- lembrando_classes_em_Python/Only_classes_Python/class_Pessoa.py
+++ lembrando_classes_em_Python/Only_classes_Python/class_Pessoa.py
@@ -21,3 +21,28 @@ print('')
 print(pessoa.idade)
 print('')
 print(pessoa.saudacao())
+
+
+print("######################################################")
+
+class Pessoa:
+    '''Classe para dar informações de uma pessoa'''
+    def __init__(self, nome, idade, endereco):
+        self.nome = nome
+        self.idade = idade
+        self.endereco = endereco
+
+    def cumprimentar(self):
+        return "Olá, meu nome é " + self.nome
+
+    def informacoes(self):
+        return "Nome: " + self.nome + ", Idade: " + str(self.idade) + ", Endereço: " + self.endereco
+
+pessoa1 = Pessoa("João", 30, "Rua das Flores, 123")
+pessoa2 = Pessoa("Maria", 25, "Rua dos Jardins, 456")
+
+print(pessoa1.cumprimentar())
+print(pessoa1.informacoes())
+
+print(pessoa2.cumprimentar())
+print(pessoa2.informacoes())
